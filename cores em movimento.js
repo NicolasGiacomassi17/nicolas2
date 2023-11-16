c4dbe07dca0d6b54bba6ec0d4b63680a3e8f39d1
NicolasGiacomassi17 <150350118+NicolasGiacomassi17@users.noreply.github.com>
2023-11-16 09:45:29 -0300

    Create cores em movimento.js

---
+++ cores em movimento.js
@@ -0,0 +1,32 @@
+let cor;
+let posicaoHoriontal;
+let poicaovertical;
+
+function setup() {
+  createCanvas(400, 400);
+  background(100,0,0);
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw() {
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal - 1;
+  }
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal + 1;
+  }
+  if (mouseY < posicaoVertical){
+    posicaoVertical = posicaoVertical -1;
+  }
+    if (mouseY > posicaoVertical) {
+      posicaoVertical = posicaoVertical + 1;
+    }
+  if (mouseIsPressed) {
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+}
